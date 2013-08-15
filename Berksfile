5403312d777270d85b871b3171d325c9ceb69aab
Joe Fitzgerald <joeyfitz@gmail.com>
2013-08-15 14:23:35 -0600

    Initial Commit

---
+++ Berksfile
@@ -0,0 +1,6 @@
+site :opscode
+
+metadata
+cookbook 'webpi', github: 'opscode-cookbooks/webpi'
+cookbook 'windows', github: 'joefitzgerald/windows'
+cookbook 'minitest-handler', github: 'btm/minitest-handler-cookbook'
---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'webpi-installer'
+maintainer       'Joe Fitzgerald'
+maintainer_email 'joe.fitzgerald@emc.com'
+license          'Apache 2.0'
+description      'Installs packages using WebPI'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+depends          'windows'
+depends          'webpi'
